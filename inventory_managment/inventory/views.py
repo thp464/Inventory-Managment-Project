be91475cfba5ba42df725c88e60abdd2b6414a5b
Thai Pham <hp.thai3@gmail.com>
2025-06-19 15:30:48 -0500

    finish login, logout, and navigation bar

--- inventory_managment/inventory/views.py
+++ inventory_managment/inventory/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm
 
 # Create your views here.
 class Index(TemplateView):
     template_name = 'inventory/index.html'
+
+class SignUpView(View):
+	def get(self, request):
+		form = UserRegistrationForm()
+		return render(request, 'inventory/signup.html', {'form': form})
+
+	def post(self, request):
+		form = UserRegistrationForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			user = authenticate(
+				username=form.cleaned_data['username'],
+				password=form.cleaned_data['password1']
+			)
+
+			login(request, user)
+			return redirect('index')
+
+		return render(request, 'inventory/signup.html', {'form': form})
--- inventory_managment/inventory_managment/settings.py
+++ inventory_managment/inventory_managment/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'inventory',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = '/dashboard'
+LOGIN_URL = 'login'
